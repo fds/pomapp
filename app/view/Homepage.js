6de6e009be6392e52bce41b1f75d39b220a2b33c
fds <345295508@qq.com>
2013-03-12 17:28:51 +0800

    add desktop

--- app/view/Homepage.js
+++ app/view/Homepage.js
@@ -7,26 +7,67 @@ Ext.define('pomapp.view.Homepage', {
     ],
     
     config: {
-        scrollable: true,
+        // fullscreen: true,
+        layout: {
+            type: 'vbox'
+        },
         
         items: [
             {
                 xtype: 'titlebar',
                 docked: 'top',
-                title: '我的第一个ShenCha程序'
+                title: '桌面'
             },
-
             {
-                styleHtmlContent: true,
-                maxWidth: 750,
-               html:[
-                    '<imgsrc="http://staging.sencha.com/img/sencha.png" />',
-                    '<h1>Welcome to Sencha Touch</h1>',
-                    "<p>You're creating the Getting Started app. This demonstrates how ",
-                    "to use tabs, lists and forms to create a simple app</p>",
-                    '<h2>Sencha Touch (2.0.0)</h2>'
-                ].join("")
-            }
+            xtype: 'panel',
+            margin: '25 0 0 0',
+            defaults: {
+                xtype: 'panel',
+                layout: 'hbox',
+                width:320,
+                margin: '25 auto 0 auto',
+            },
+            items: [{
+                defaults: {
+                    xtype: 'panel',
+                    margin: 10,
+                    style: 'border: 1px solid #cccccc; border-radius: 5px;'
+                },
+                items: [{
+                    html: '<img src="resources/images/1.jpg" width="85" height="85" />'
+                }, {
+                    html: '<img src="resources/images/2.jpg" width="85" height="85" />'
+                }, {
+                    html: '<img src="resources/images/3.jpg" width="85" height="85" />'
+                }]
+            }, {
+                defaults: {
+                    xtype: 'panel',
+                    margin: 10,
+                    style: 'border: 1px solid #cccccc; border-radius: 5px;'
+                },
+                items: [{
+                    html: '<img src="resources/images/4.jpg" width="85" height="85" />'
+                }, {
+                    html: '<img src="resources/images/5.jpg" width="85" height="85" />'
+                }, {
+                    html: '<img src="resources/images/6.jpg" width="85" height="85" />'
+                }]
+            }, {
+                defaults: {
+                    xtype: 'panel',
+                    margin: 10,
+                    style: 'border: 1px solid #cccccc; border-radius: 5px;'
+                },
+                items: [{
+                     html: '<img src="resources/images/7.jpg" width="85" height="85" />'
+                }, {
+                     html: '<img src="resources/images/8.jpg" width="85" height="85" />'
+                }, {
+                     html: '<img src="resources/images/9.jpg" width="85" height="85" />'
+                }]
+            }]
+        }
         ]
     }
 });
